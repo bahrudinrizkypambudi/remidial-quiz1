48e2a4d1a39e09f42520c13ac510787acb20e16f
Sintongs <Sintongs@DESKTOP-N0EMNOQ>
2018-11-04 22:13:01 +0700

    perubahan

---
+++ src/kamera.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sintongs
+ */
+public class kamera {
+    private String merk;
+    private int biaya:
+    
+    kamera(){
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+    
+    
+}
