593a8ce4a2d23e245cb8b3a0b73512b4a5f042e0
Bahrudin <bahrudinrizky313@gmail.com>
2018-11-04 23:48:39 +0700

    perubahan

---
+++ src/Pelanggan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bahrudin<bahrudinrizky313@gmail.com>
+ */
+public class Pelanggan {
+    private String nama;
+    private int hari;
+
+    public Pelanggan(String nama, int hari) {
+        this.nama = nama;
+        this.hari = hari;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getHari() {
+        return hari;
+    }
+
+    public void setHari(int hari) {
+        this.hari = hari;
+    }
+}
